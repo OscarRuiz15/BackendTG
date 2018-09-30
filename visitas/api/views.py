d4d83186ab00804e774efea3b0955201319249ea
Oscar Ruiz <ruiz.oscar@correounivalle.edu.co>
2018-09-30 01:58:27 -0500

    Correccion de modelo, busqueda de visitas por uid de usuario e id de lugar, error a la muestra de datos

--- visitas/api/views.py
+++ visitas/api/views.py
@@ -17,7 +17,15 @@ class VisitasListView(mixins.CreateModelMixin, generics.ListAPIView):
     serializer_class = VisitaSerializer
 
     def get_queryset(self):
-        return Visita.objects.all()
+        qs = Visita.objects.all()
+        query = self.request.GET.get("usuario")
+        if query is not None:
+            qs = qs.filter(Q(usuario__uid=query)).distinct()
+        else:
+            query = self.request.GET.get("lugar")
+            if query is not None:
+                qs = qs.filter(Q(lugar__id=query)).distinct()
+        return qs
 
     def post(self, request, *args, **kwargs):
        return self.create(request, *args, **kwargs)
