3a1aa0d061b7eafae363158b01f2211d9bbf2a8e
Oscar Ruiz <ruiz.oscar@correounivalle.edu.co>
2018-10-02 12:58:53 -0500

    Modificación modelo y serializers eventos

--- eventos/api/views.py
+++ eventos/api/views.py
@@ -3,13 +3,13 @@ from eventos.models import Evento
 from .serializers import EventoSerializer
 from django.db.models import Q
 
-
 class EventoView(generics.RetrieveUpdateAPIView):
     lookup_field = 'id'
     serializer_class = EventoSerializer
 
     def get_queryset(self):
         return Evento.objects.all()
+
 
 class EventoListView(mixins.CreateModelMixin,generics.ListAPIView):
     lookup_field = 'id'
@@ -20,6 +20,7 @@ class EventoListView(mixins.CreateModelMixin,generics.ListAPIView):
         query = self.request.GET.get("lugar")
         if query is not None:
             qs = qs.filter(Q(lugar__id=query)).distinct()
+        return qs
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
