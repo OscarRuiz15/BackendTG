93626b90995da4abe76d52a3d9bdd959105deff2
Oscar Ruiz <ruiz.oscar@correounivalle.edu.co>
2018-10-02 15:20:29 -0500

    Modificación modelo solicitudes

--- solicitudes/models.py
+++ solicitudes/models.py
@@ -7,11 +7,11 @@ from usuarios.models import Usuario
 
 class Solicitud(models.Model):
     id = models.AutoField(primary_key=True)
-    usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     nombre_lugar=models.CharField(max_length=100)
     direccion=models.CharField(max_length=50)
     telefono=models.CharField(max_length=15)
-    email = ArrayField(ArrayField(models.CharField(max_length=50, blank=True)))
+    email = ArrayField(models.CharField(max_length=50, blank=True))
     informacion = models.CharField(max_length=200)
     nit = models.IntegerField()
     aceptado = models.BooleanField(default=False)
