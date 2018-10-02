24ac1645dfc24020bbf79efea0d01bd88af7115f
Oscar Ruiz <ruiz.oscar@correounivalle.edu.co>
2018-10-02 15:55:10 -0500

    Modificación modelo visitas

--- visitas/models.py
+++ visitas/models.py
@@ -7,8 +7,8 @@ from usuarios.models import Usuario
 
 class Visita(models.Model):
     id = models.AutoField(primary_key=True)
-    usuario = models.ManyToManyField(Usuario)
-    lugar = models.ManyToManyField(Lugar)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    lugar = models.ForeignKey(Lugar, on_delete=models.CASCADE)
 
     def __unicode__(self):
         return self.lugar.nombre
