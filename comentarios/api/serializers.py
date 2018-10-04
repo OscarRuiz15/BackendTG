5f9587f9971b64771b1a2d0cf365f91b4f680bfa
andresFMedina <andres.medina@correounivalle.edu.co>
2018-10-03 20:57:54 -0500

    Cambios en el serializer

--- comentarios/api/serializers.py
+++ comentarios/api/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from comentarios.models import Comentario
+from usuarios.api.serializers import UsuarioSerializer
+from usuarios.models import Usuario
 
 
-class ComentarioSerializer(serializers.HyperlinkedModelSerializer):
+class ComentarioSerializer(serializers.ModelSerializer):
+    usuario = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='nombre',
+    )
+
     class Meta:
         model = Comentario
         fields = ['id',
