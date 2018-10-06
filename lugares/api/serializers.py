0f074a2a794ba28b11fdbc9ea2b683de1444dc2f
Oscar Ruiz <ruiz.oscar@correounivalle.edu.co>
2018-10-05 22:27:41 -0500

    Cambios serializers lugar

--- lugares/api/serializers.py
+++ lugares/api/serializers.py
@@ -1,14 +1,11 @@
-from rest_framework import serializers
+from drf_writable_nested import WritableNestedModelSerializer
 from lugares.models import Lugar
 from productos.api.serializers import ProductoSerializer
-from productos.models import Producto
 from tags.api.serializers import TagsSerializer
-from tags.models import Tag
 from comentarios.api.serializers import ComentarioSerializer
-from comentarios.models import Comentario
 
 
-class LugarSerializer(serializers.ModelSerializer):
+class LugarSerializer(WritableNestedModelSerializer):
     producto = ProductoSerializer(many=True)
     tag = TagsSerializer(many=True)
     comentario = ComentarioSerializer(many=True)
@@ -39,24 +36,24 @@ class LugarSerializer(serializers.ModelSerializer):
                   'fecha_creacion',
         ]
 
-        def create(self, validated_data):
-            productos_data = validated_data.pop('producto')
-            lugar = Lugar.objects.create(**validated_data)
-            for producto_data in productos_data:
-                Producto.objects.create(lugar=lugar, **producto_data)
-            return lugar
-
-        def create(self, validated_data):
-            tags_data = validated_data.pop('tag')
-            lugar = Lugar.objects.create(**validated_data)
-            for tag_data in tags_data:
-                Producto.objects.create(lugar=lugar, **tag_data)
-            return lugar
-
-        def create(self, validated_data):
-            comentarios_data = validated_data.pop('comentario')
-            lugar = Lugar.objects.create(**validated_data)
-            for comentario_data in comentarios_data:
-                Producto.objects.create(lugar=lugar, **comentario_data)
-            return lugar
+        # def create(self, validated_data):
+        #     productos_data = validated_data.pop('producto')
+        #     lugar = Lugar.objects.create(**validated_data)
+        #     for producto_data in productos_data:
+        #         Producto.objects.create(lugar=lugar, **producto_data)
+        #     return lugar
+        #
+        # def create(self, validated_data):
+        #     tags_data = validated_data.pop('tag')
+        #     lugar = Lugar.objects.create(**validated_data)
+        #     for tag_data in tags_data:
+        #         Producto.objects.create(lugar=lugar, **tag_data)
+        #     return lugar
+        #
+        # def create(self, validated_data):
+        #     comentarios_data = validated_data.pop('comentario')
+        #     lugar = Lugar.objects.create(**validated_data)
+        #     for comentario_data in comentarios_data:
+        #         Producto.objects.create(lugar=lugar, **comentario_data)
+        #     return lugar
 
