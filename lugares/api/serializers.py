4b5e4221547fd73f02678ab0f0fbcb9f0defb25f
andresFMedina <andres.medina@correounivalle.edu.co>
2018-12-16 14:14:15 -0500

    Correccion al serializer

--- lugares/api/serializers.py
+++ lugares/api/serializers.py
@@ -1,19 +1,20 @@
 from drf_writable_nested import WritableNestedModelSerializer
 from rest_framework import serializers
+
+from comentarios.api.serializers import ComentarioSerializer
 from departamentos.api.serializers import DepartamentoSerializer
 from lugares.models import Lugar
 from municipios.api.serializers import MunicipioSerializer
 from productos.api.serializers import ProductoSerializer
 from tags.api.serializers import TagsSerializer
-from comentarios.api.serializers import ComentarioSerializer
 
 
 class LugarSerializer(WritableNestedModelSerializer, serializers.ModelSerializer):
     producto = ProductoSerializer(many=True)
     tag = TagsSerializer(many=True)
     comentario = ComentarioSerializer(many=True)
-    departamento = DepartamentoSerializer()
-    municipio = MunicipioSerializer()
+    departamento = DepartamentoSerializer(read_only=True)
+    municipio = MunicipioSerializer(read_only=True)
 
     class Meta:
         model = Lugar
@@ -41,6 +42,4 @@ class LugarSerializer(WritableNestedModelSerializer, serializers.ModelSerializer
                   'fecha_creacion',
                   'departamento',
                   'municipio'
-        ]
-
-
+                  ]
