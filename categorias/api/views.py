bb83eebea4f70d511263d4ed73c28c722e6ba337
andresFMedina <andres.medina@correounivalle.edu.co>
2019-01-08 11:51:54 -0500

    Permisos faltantes

--- categorias/api/views.py
+++ categorias/api/views.py
@@ -3,52 +3,29 @@ from firebase_admin import credentials, auth
 from rest_framework import generics, mixins
 from rest_framework.renderers import JSONRenderer
 
+from BackendTG.permisos import AuthFirebaseUser
 from categorias.models import Categoria
 from .serializers import CategoriasSerializer
 
 
 class CategoriasView(generics.RetrieveAPIView):
     lookup_field = 'id'
-    serializer_class =  CategoriasSerializer
+    serializer_class = CategoriasSerializer
     renderer_classes = (JSONRenderer,)
+    permission_classes = (AuthFirebaseUser,)
 
     def get_queryset(self):
-        try:
-            token = self.request.META['HTTP_AUTHORIZATION']
-            print(token)
-            decoded_token = auth.verify_id_token(token)
-            uid = decoded_token['uid']
-            print(uid)
-            return Categoria.objects.all()
-        except:
-            return None
+        return Categoria.objects.all()
 
 
-
-
-class CategoriasListView(mixins.CreateModelMixin,generics.ListAPIView):
+class CategoriasListView(mixins.CreateModelMixin, generics.ListAPIView):
     lookup_field = 'id'
     serializer_class = CategoriasSerializer
     renderer_classes = (JSONRenderer,)
+    permission_classes = (AuthFirebaseUser,)
 
     def get_queryset(self):
-        try:
-            token = self.request.META['HTTP_AUTHORIZATION']
-            print(token)
-            decoded_token = auth.verify_id_token(token)
-            uid = decoded_token['uid']
-            print(uid)
-            return Categoria.objects.all()
-        except:
-            return None
+        return Categoria.objects.all()
 
-    def post(self,request,*args,**kwargs):
-        try:
-            token = self.request.META['HTTP_AUTHORIZATION']
-            print(token)
-            decoded_token = auth.verify_id_token(token)
-            uid = decoded_token['uid']
-            print(uid)
-            return self.create(request, *args, **kwargs)
-        except:
-            return None
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
