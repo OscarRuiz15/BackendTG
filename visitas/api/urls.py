291a2ad912e8ec21ff54f79b19e4fbf13800393f
Oscar Ruiz <oarp1996@hotmail.com>
2019-01-25 22:36:11 -0500

    Se crea archivo test para visitas, falta probar y modificar

--- visitas/api/urls.py
+++ visitas/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import VisitasView, VisitasListView, VisitasUsuarioCount
 
 urlpatterns = [
-    path('', VisitasListView.as_view(), name='users-create'),
-    path('count/', VisitasUsuarioCount.as_view(), name='users-create'),
-    path('<int:id>/', VisitasView.as_view(), name='users-rud'),
+    path('', VisitasListView.as_view(), name='visitas-create'),
+    path('count/', VisitasUsuarioCount.as_view(), name='visitas-count'),
+    path('<int:id>/', VisitasView.as_view(), name='visitas-rud'),
 ]
--- visitas/models.py
+++ visitas/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from rest_framework.reverse import reverse as api_reverse
 
 # Create your models here.
 from lugares.models import Lugar
@@ -11,6 +12,9 @@ class Visita(models.Model):
     lugar = models.ForeignKey(Lugar, on_delete=models.CASCADE)
     fecha_visita = models.DateField()
     hora_visita = models.TimeField()
+
+    def get_api_url(self, request=None):
+        return api_reverse("api-visitas:visitas-rud", kwargs={'id': self.id}, request=request)
 
     def __unicode__(self):
         return self.lugar.nombre
