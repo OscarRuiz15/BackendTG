a1c5d20fa050719d62c5248f445d66ae6fb57012
andresFMedina <andres.medina@correounivalle.edu.co>
2019-02-14 10:59:30 -0500

    Se agrega la peticion DELETE a las solicitudes

--- solicitudes/api/views.py
+++ solicitudes/api/views.py
@@ -1,12 +1,13 @@
 from rest_framework import generics, mixins
 from rest_framework.renderers import JSONRenderer
 
+from django.db.models import Q
 from BackendTG.permisos import AuthFirebaseUser
 from solicitudes.models import Solicitud
 from .serializers import SolicitudSerializer
 
 
-class SolicitudesView(generics.RetrieveUpdateAPIView):
+class SolicitudesView(generics.RetrieveUpdateDestroyAPIView):
     lookup_field = 'id'
     serializer_class = SolicitudSerializer
     renderer_classes = (JSONRenderer,)
@@ -16,14 +17,21 @@ class SolicitudesView(generics.RetrieveUpdateAPIView):
         return Solicitud.objects.all()
 
 
-class SolictidudesListView(mixins.CreateModelMixin, generics.ListAPIView):
+class SolicitudesListView(mixins.CreateModelMixin, generics.ListAPIView):
     lookup_field = 'id'
     serializer_class = SolicitudSerializer
     renderer_classes = (JSONRenderer,)
     permission_classes = (AuthFirebaseUser,)
 
     def get_queryset(self):
-        return Solicitud.objects.all()
+        qs = Solicitud.objects.all()
+        query = self.request.GET.get("usuario")
+        if query is not None:
+            qs = qs.filter(Q(usuario__uid__exact=query))
+        return qs
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
+
+
+
