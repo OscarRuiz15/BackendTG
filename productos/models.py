20675895998871db9a5054b3a7ecaceaf4eb543e
Oscar Ruiz <ruiz.oscar@correounivalle.edu.co>
2019-03-19 22:42:54 -0500

    Cambios en el modelo de productos, el precio no es obligatorio

--- productos/models.py
+++ productos/models.py
@@ -9,7 +9,7 @@ class Producto(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
     descripcion = models.CharField(max_length=200)
-    precio = models.CharField(max_length=20)
+    precio = models.CharField(max_length=20, blank=True)
     foto = models.CharField(max_length=100)
     lugar = models.ForeignKey(Lugar, on_delete=models.CASCADE)
 
