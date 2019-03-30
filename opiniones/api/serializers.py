77c16be6275343bf5d4b3cc02b1fa9a4577a76a5
Oscar Ruiz <ruiz.oscar@correounivalle.edu.co>
2019-03-30 14:13:42 -0500

    Cambio permisos y serializer de opiniones

--- opiniones/api/serializers.py
+++ opiniones/api/serializers.py
@@ -1,18 +1,7 @@
 from drf_writable_nested import WritableNestedModelSerializer
-from rest_framework import serializers
 from opiniones.models import Opinion
-from usuarios.models import Usuario
-
-class UsuarioSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Usuario
-        fields = ['id',
-                  'nombre',
-                  'foto',
-                  ]
 
 class OpinionSerializer(WritableNestedModelSerializer):
-    usuario = UsuarioSerializer()
 
     class Meta:
         model = Opinion
@@ -22,10 +11,3 @@ class OpinionSerializer(WritableNestedModelSerializer):
                   'like'
                   ]
 
-        def create(self, validated_data):
-            user_data = validated_data.pop('usuario')
-            user = UsuarioSerializer.create(UsuarioSerializer(), validated_data=user_data)
-            opinion, created = Opinion.objects.update_or_create(usuario=user,
-                                                                lugar=validated_data.pop('lugar'),
-                                                                like=validated_data.pop('like'))
-            return opinion
